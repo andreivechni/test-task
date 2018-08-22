eb4fc1dc136f212b516f02d829f45169188241f4
reddylime <krasnijlajm@gmail.com>
2018-08-22 17:54:18 +0300

    random3

--- middleware/tokenCheck.js
+++ middleware/tokenCheck.js
@@ -2,6 +2,7 @@ const config = require('../config/config');
 
 module.exports = function (req, res, next) {
     if(!config.token) {
+        console.log('token check');
         res.redirect('http://vast-springs-36717.herokuapp.com');
     } else {
         next();
